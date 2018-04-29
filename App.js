2bc5abc7ec268a6cb8d89c7ced5ce18e266cb8d0
liyang <1711467488@qq.com>
2018-04-29 12:10:15 +0800

    no message

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+class Header extends Component {
+  static defaultProps = {
+    title: 'Header default'
+  }
+  render () {
+    return (
+      <View style={styles.header}>
+        <Text>{this.props.title}</Text>
+      </View>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+      <View style={styles.main}>
+        <Text>main</Text>
+      </View>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Header></Header>
+        <Main></Main>
       </View>
     );
   }
@@ -14,8 +38,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 45
   },
+  header: {
+    height: 60,
+    backgroundColor: '#abcdef'
+  },
+  main: {
+    flex: 1,
+    height: 60,
+    backgroundColor: '#CCC'
+  }
 });
