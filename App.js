cfb254c3b0eac29cfe211f3ca74b62c3851f6e2d
liyang <1711467488@qq.com>
2018-04-29 15:43:25 +0800

    no message

--- App.js
+++ App.js
@@ -1,69 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-// 顶栏
-class Header extends Component {
-  static defaultProps = {
-    title: 'Header default'
-  }
-  render () {
-    return (
-      <View style={styles.header}>
-        <Text style={styles.headerText}>
-          {this.props.title}
-        </Text>
-      </View>
-    )
-  }
-}
-
-// 主体布局
-class Main extends Component {
-  render () {
-    return (
-      <View style={styles.main}>
-        <Text>main</Text>
-      </View>
-    )
-  }
-}
-
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 // APP
-export default class App extends React.Component {
+export default class extends Component {
+  state = {
+    like: 0
+  }
+  handlePress = () => {
+    this.setState({
+      like: this.state.like + 1
+    })
+  }
   render() {
+    const { like } = this.state
     return (
       <View style={styles.container}>
-        <Header title="Hello World" />
-        <Main />
+        <TouchableOpacity onPress={this.handlePress}>
+          <Image source={require('./assets/like.png')} />
+        </TouchableOpacity>
+        <Text style={styles.title}>
+          Like {like}
+        </Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // 最外层容器
   container: {
-    flex: 1
-  },
-  // 顶栏
-  header: {
-    height: 60,
-    borderBottomWidth: 0.5,
-    borderBottomColor: '#FEA53B',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20,
-    backgroundColor: '#FECC3B'
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#FFF'
-  },
-  // 主体
-  main: {
     flex: 1,
-    height: 60,
-    backgroundColor: '#FFF'
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 60
   }
 });
